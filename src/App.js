fff7121ff3398b3786e1253bc36bce54170c417b
angelsol299 <angelsol299@hotmail.com>
2018-11-26 21:18:08 +0100

    side drawer functional

--- src/App.js
+++ src/App.js
@@ -14,17 +14,20 @@ class App extends Component {
     });
   };
 
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
   render() {
     let sideDrawer;
     let backdrop;
     if (this.state.sideDrawerOpen) {
-      sideDrawer = <SideDrawer />;
-      backdrop = <Backdrop />;
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
     return (
       <div style={{ height: "100%" }}>
-        <Toolbar drawerToggleClickHandler={this.drawerToggleClickHandler} />
-        {sideDrawer}
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <sideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
         <main style={{ marginTop: "63px" }}>
           <p>this is the page content</p>
