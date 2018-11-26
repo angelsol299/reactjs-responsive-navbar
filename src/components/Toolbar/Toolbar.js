5e76b4786d61906fdb28abadc18e8a28fc1cf9b9
angelsol299 <angelsol299@hotmail.com>
2018-11-26 20:12:38 +0100

    first elements of the navbar with components and css files

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,25 @@
 import React from "react";
+import "./Toolbar.css";
 
 const toolbar = props => (
-  <header>
-    <nav>
+  <header className="toolbar">
+    <nav className="toolbar-navigation">
       <div />
-      <div>
+      <div className="toolbar-logo">
         <a href="/">The Logo</a>
       </div>
-      <div />
+      <div className="toolbar-navigation-items">
+        <ul>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 );
+
+export default toolbar;
