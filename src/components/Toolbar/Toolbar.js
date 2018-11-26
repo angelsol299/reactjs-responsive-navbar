e593ad637e29ac42adc17d4a23cf93046916c51d
angelsol299 <angelsol299@hotmail.com>
2018-11-26 20:00:53 +0100

    first components and nav structures

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const toolbar = props => (
+  <header>
+    <nav>
+      <div />
+      <div>
+        <a href="/">The Logo</a>
+      </div>
+      <div />
+    </nav>
+  </header>
+);
