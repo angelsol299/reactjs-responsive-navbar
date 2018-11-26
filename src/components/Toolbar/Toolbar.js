6779a8500cab54e41b9f1b874de74392bc104699
angelsol299 <angelsol299@hotmail.com>
2018-11-26 20:23:44 +0100

    navbar ready and adding button to make it responsive

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -8,6 +8,7 @@ const toolbar = props => (
       <div className="toolbar-logo">
         <a href="/">The Logo</a>
       </div>
+      <div className="spacer" />
       <div className="toolbar-navigation-items">
         <ul>
           <li>
